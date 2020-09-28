5143a536514ccf33218e2dd231cdbffbaa450851
maartynaa <maartynaa@example.com>
2020-09-28 14:27:31 +0200

    Send email in custom module

--- app/code/Bulbulatory/Recomendations/Controller/Customer/Index.php
+++ app/code/Bulbulatory/Recomendations/Controller/Customer/Index.php
@@ -1,15 +1,20 @@
-<?php 
+<?php
+namespace Bulbulatory\Recomendations\Controller\Customer;
 
-namespace Bulbulatory\Recomendations\Controller\Customer;  
+class Index extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
 
-use Magento\Framework\App\Action\Action;
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
 
-class Index extends Action { 
-  
-    public function execute() { 
-     
-       $this->_view->loadLayout(); 
-       $this->_view->renderLayout(); 
-    } 
-     
-} 
+	public function execute()
+	{
+		return $this->_pageFactory->create();
+	}
+}
