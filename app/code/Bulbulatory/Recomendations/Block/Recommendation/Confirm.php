89a58bbd3c659525f5f198e46439a13cccb6b609
maartynaa <maartynaa@example.com>
2020-09-30 11:30:27 +0200

    Create controller to confirm recommendation

---
+++ app/code/Bulbulatory/Recomendations/Block/Recommendation/Confirm.php
@@ -0,0 +1,26 @@
+<?php
+ 
+namespace Bulbulatory\Recomendations\Block\Recommendation;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Block\Product\Context;
+ 
+class Confirm extends Template {
+ 
+    public function __construct(Context $context, array $data = []) {
+ 
+        parent::__construct($context, $data);
+ 
+    }
+
+    // public function getFormAction()
+    // {
+    //     return '/recomendations/customer/post';
+    // }
+
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+ 
+}
