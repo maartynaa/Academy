ba28535e55910695908198f32e24f25bb8b34f0e
maartynaa <maartynaa@example.com>
2020-10-13 10:47:37 +0200

    Task 10 Fix add custom discount

--- app/code/Bulbulatory/Recomendations/view/frontend/web/js/view/checkout/summary/customdiscount.js
+++ app/code/Bulbulatory/Recomendations/view/frontend/web/js/view/checkout/summary/customdiscount.js
@@ -2,19 +2,23 @@ define(
     [
         'jquery',
         'Magento_Checkout/js/view/summary/abstract-total',
-        'Magento_Checkout/js/model/quote'
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/totals'
     ],
-    function ($,Component, quote) {
+    function ($,Component, quote, totals) {
         "use strict";
         return Component.extend({
             defaults: {
                 template: 'Bulbulatory_Recomendations/checkout/summary/customdiscount'
             },
             totals: quote.getTotals(),
+
             isDisplayedCustomdiscount : function(){
                 return true;
             },
             getCustomDiscount : function(){
+
+                console.log(totals);
 
                 var price = totals.getSegment('customer_discount').value;
                 return this.getFormattedPrice(price);
